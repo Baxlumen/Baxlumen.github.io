a17ad5047b1e09f3ac41f2cf56c36dae74ab7b1e
Baxlumen <1546611349@qq.com>
2019-05-09 10:12:10 +0800

    Site updated: 2019-05-09 10:12:06

---
+++ dist/music.js
@@ -0,0 +1,15 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    listFolded: false,
+    audio: [
+      {
+        name: "小半",
+        artist: '陈粒',
+        url: 'http://www.ytmp3.cn/down/52276.mp3',
+        cover: '',
+      },
+      
+    ]
+});
